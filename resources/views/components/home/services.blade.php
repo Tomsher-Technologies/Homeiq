42e36513f2371b5f035e38d77cb0a5913c02fbb7
suhailtomsher <suhail@tomsher.com>
2025-04-22 20:25:52 +0400

    responsive

--- resources/views/components/home/services.blade.php
+++ resources/views/components/home/services.blade.php
@@ -1,7 +1,5 @@
-
-
 <section id="services-section" class="pt-10 pb-10 bg-gray-50">
-    <div class="container mx-auto px-6 lg:px-12">
+    <div class="container-lg mx-auto px-6 lg:px-12">
         <!-- Section Title -->
         <h2 class="text-3xl md:text-4xl font-normal text-center relative z-10">
             @php
