36530843353a08431011e26fe017e264f058cbdd
Jishap-Tomsher <130639775+Jishap-Tomsher@users.noreply.github.com>
2025-04-24 12:05:54 +0400

    footer mobile changes

--- resources/views/components/navigation/footer.blade.php
+++ resources/views/components/navigation/footer.blade.php
@@ -93,8 +93,9 @@
         <div class="flex flex-col md:flex-row md:justify-between md:items-center border-b border-gray-600 pb-6">
             <!-- Logo & Tagline -->
             <div class="flex flex-col items-center md:items-start space-y-2">
-                <img src="{{ get_setting('footer_logo') ? uploaded_asset(get_setting('footer_logo')) : asset('images/logo.svg') }}"
-                    alt="HomeIQ Logo" class="h-10">
+                <a href="{{ route('home') }}">
+                    <img src="{{ get_setting('footer_logo') ? uploaded_asset(get_setting('footer_logo')) : asset('images/logo.svg') }}" alt="HomeIQ Logo" class="h-10">
+                </a>
                 <p class="text-white text-sm">{!! get_setting('about_us_description', null, $lang) !!}</p>
             </div>
 
@@ -205,4 +206,69 @@
         </div>
 
     </div>
+
+    
+    <!-- Mobile Bottom Navigation -->
+    <div class="hidden fixed z-30 bottom-0 bg-primary flex items-center justify-between w-full h-16 px-3 sm:px-10">
+        <!-- Menu Icon -->
+        <button id="menu-button" class="flex items-center justify-center h-auto relative focus:outline-none menu-icon">
+        <span class="text-xl text-white">
+            <svg class="w-6 h-6 drop-shadow-xl" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none"
+            stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+            <line x1="17" y1="10" x2="3" y2="10"></line>
+            <line x1="21" y1="6" x2="3" y2="6"></line>
+            <line x1="21" y1="14" x2="3" y2="14"></line>
+            <line x1="17" y1="18" x2="3" y2="18"></line>
+            </svg>
+        </span>
+        </button>
+
+        <!-- Home Icon -->
+        <a class="text-xl text-white" rel="noreferrer" aria-label="Home" href="{{ route('home') }}">
+        <svg class="w-6 h-6 drop-shadow-xl" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none"
+            stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+        </svg>
+        </a>
+
+        <!-- Cart Icon -->
+        <button class="h-9 w-9 relative inline-flex items-center justify-center text-white text-lg">
+        <svg class="w-6 h-6 drop-shadow-xl" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none"
+            stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+            <circle cx="9" cy="21" r="1"></circle>
+            <circle cx="20" cy="21" r="1"></circle>
+            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+        </svg>
+        </button>
+
+        <!-- User Icon -->
+        <button type="button" class="text-xl h-9 w-9 text-white indicator justify-center">
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+            width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        </svg>
+
+        </button>
+    </div>
+
+    <!-- Drawer Menu -->
+    <div id="drawer-menu" class="fixed hidden top-0 left-0 w-64 h-full bg-white shadow-xl z-50 p-5">
+        <button id="close-menu" class="text-xl absolute top-5 right-5">
+        <svg class="w-6 h-6 text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"
+            stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+        </button>
+
+        <!-- Insert menu here -->
+
+    </div>
+
+    <!-- Drawer Overlay -->
+    <div id="drawer-overlay" class="fixed inset-0 bg-black opacity-50 z-40 hidden"></div>
+
 </footer>
+
+ 
